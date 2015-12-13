463600386586aa6ca2c1f9f23bd2a991d8b1162a
Michael <Mikeks81@gmail.com>
2015-12-12 22:41:33 -0500

    added function to add to to-do list

--- script.js
+++ script.js
@@ -1,13 +1,23 @@
 $(document).ready(
 	function(){
 
-		var months = ["","January","February","March","April",
+		var months = ["January","February","March","April",
 						"May","June","July","August","September",
 						"October","November","December"]
-		var date = new Date();
-		var day = date.getDay();
-		var month = date.getMonth();
-		var year = date.getFullYear();
-		$("h4").text(months[month]+ " " + day + ", " + year);
+		var date = new Date(); // new date instance in numerical form
+		var day = date.getDate(); // numerical form of day of the month
+		var month = date.getMonth(); // numerical form of month of the year
+		var year = date.getFullYear(); // full 4 digit numerical form of year
+		$("h4").text(months[month]+ " " + day + ", " + year); //the value of month displays item in months Array to screen
 
-	});
+		$("#add_button").on('click',
+			function(e){
+				e.preventDefault();
+				var id = "item" 
+				var user_input = $("#input").val();
+				$("#list").append('<input type="checkbox">' + user_input + '</input></br>');
+				
+
+		});		
+
+});
